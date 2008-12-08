0846f1c9a6eab45fe305b3b4be829d4a89a722de
Tekkub <tekkub@gmail.com>
2008-12-08 15:03:32 -0700

    LDB data source

--- picoFPS.lua
+++ picoFPS.lua
@@ -26,7 +26,7 @@ end
 -------------------------------------------
 
 local f = CreateFrame("frame")
-local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("picoFPS", {text = "75.0 FPS", OnClick = function() collectgarbage("collect") end})
+local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("picoFPS", {type = "data source", text = "75.0 FPS", OnClick = function() collectgarbage("collect") end})
 local about = LibStub("tekKonfig-AboutPanel").new(nil, "picoFPS")
 
 
